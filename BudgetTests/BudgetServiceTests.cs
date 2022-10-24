69110c531b49b276d3b682b8a5e8730210ac1d13
Tintin <yuijuhn.ting@titansoft.com.sg>
2022-10-25 00:44:19 +0800

    refactor: move GivenBudgets to setup

--- BudgetTests/BudgetServiceTests.cs
+++ BudgetTests/BudgetServiceTests.cs
@@ -15,20 +15,19 @@ public class BudgetServiceTests
     {
         _budgetRepo = Substitute.For<IBudgetRepo>();
         _budgetService = new BudgetService(_budgetRepo);
+        
+        GivenBudgets(new List<Budget>
+        {
+            CreateBudget("202209", 31),
+            CreateBudget("202210", 310000),
+            CreateBudget("202211", 3000),
+            CreateBudget("202212", 31),
+        });
     }
 
     [Test]
     public void one_month()
     {
-        _budgetRepo.GetAll().Returns(new List<Budget>
-        {
-            new()
-            {
-                YearMonth = "202210",
-                Amount = 310000
-            }
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 1), new DateTime(2022, 10, 31));
         Assert.That(amount, Is.EqualTo(310000m));
     }
@@ -36,11 +35,6 @@ public class BudgetServiceTests
     [Test]
     public void one_day()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 310000)
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 1), new DateTime(2022, 10, 1));
         Assert.That(amount, Is.EqualTo(10000m));
     }
@@ -48,11 +42,6 @@ public class BudgetServiceTests
     [Test]
     public void cross_days()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 310000)
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 1), new DateTime(2022, 10, 6));
         Assert.That(amount, Is.EqualTo(60000m));
     }
@@ -60,26 +49,13 @@ public class BudgetServiceTests
     [Test]
     public void cross_months()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 310000),
-            CreateBudget("202211", 30000)
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 30), new DateTime(2022, 11, 5));
-        Assert.That(amount, Is.EqualTo(20000m + 5000m));
+        Assert.That(amount, Is.EqualTo(20000m + 500m));
     }
 
     [Test]
     public void cross_three_months()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 310000),
-            CreateBudget("202211", 3000),
-            CreateBudget("202212", 31)
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 30), new DateTime(2022, 12, 1));
         Assert.That(amount, Is.EqualTo(20000m + 3000m + 1m));
     }
@@ -87,28 +63,13 @@ public class BudgetServiceTests
     [Test]
     public void cross_four_months()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 31),
-            CreateBudget("202211", 3000),
-            CreateBudget("202212", 310000),
-            CreateBudget("202301", 31),
-        });
-
-        var amount = _budgetService.Query(new DateTime(2022, 10, 1), new DateTime(2023, 1, 30));
-        Assert.That(amount, Is.EqualTo(31 + 310000m + 3000m + 30m));
+        var amount = _budgetService.Query(new DateTime(2022, 9, 1), new DateTime(2022, 12, 31));
+        Assert.That(amount, Is.EqualTo(31 + 310000m + 3000m + 31m));
     }
 
     [Test]
     public void illegal_date()
     {
-        GivenBudget(new List<Budget>
-        {
-            CreateBudget("202210", 310000),
-            CreateBudget("202211", 3000),
-            CreateBudget("202212", 31)
-        });
-
         var amount = _budgetService.Query(new DateTime(2022, 10, 30), new DateTime(2022, 1, 5));
         Assert.That(amount, Is.EqualTo(0));
     }
@@ -116,9 +77,7 @@ public class BudgetServiceTests
     [Test]
     public void db_no_data()
     {
-        GivenBudget(new List<Budget>());
-
-        var amount = _budgetService.Query(new DateTime(2022, 10, 30), new DateTime(2022, 11, 5));
+        var amount = _budgetService.Query(new DateTime(2000, 10, 30), new DateTime(2000, 11, 5));
         Assert.That(amount, Is.EqualTo(0));
     }
 
@@ -131,7 +90,7 @@ public class BudgetServiceTests
         };
     }
 
-    private ConfiguredCall GivenBudget(List<Budget> returnThis)
+    private ConfiguredCall GivenBudgets(List<Budget> returnThis)
     {
         return _budgetRepo.GetAll().Returns(returnThis);
     }
